0dc608b44215012d30daff0a22dde4b3e811725a
RobertS <rshugart@protonmail.com>
2019-02-04 17:27:24 -0500

    Seasons code along

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    //This is the only time we do direct assignment.
+    this.state = { lat: null }; // If we don't know what the value of a number will be, use null unitl we know.
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    //Find users physical location with geolocation api in all browsers
+    window.navigator.geolocation.getCurrentPosition(
+      //success call back-found location
+      position => {
+        this.setState({ lat: position.coords.latitude }); //Used setState we found in
+      },
+      //Failure call back
+      err => console.log(err)
+    );
+  }
+
+  //React says we have to define render!!
+  render() {
+    return <div>Latitude: {this.state.lat}</div>;
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
