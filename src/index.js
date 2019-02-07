5811da5d0dbc370436346d1bdd50995c111c1f37
RobertS <rshugart@protonmail.com>
2019-02-07 12:35:58 -0500

    Added Spinner

--- src/index.js
+++ src/index.js
@@ -1,26 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    //This is the only time we do direct assignment.
-    this.state = { lat: null }; // If we don't know what the value of a number will be, use null unitl we know.
+  state = { lat: null, errorMessage: "" }; // If we don't know what the value of a number will be, use null and the value will be passed in once it is determined.
 
+  componentDidMount() {
     //Find users physical location with geolocation api in all browsers
     window.navigator.geolocation.getCurrentPosition(
       //success call back-found location
-      position => {
-        this.setState({ lat: position.coords.latitude }); //Used setState we found in
-      },
+      position => this.setState({ lat: position.coords.latitude }),
       //Failure call back
-      err => console.log(err)
+      err => this.setState({ errorMessage: err.message })
     );
+  }
+
+  renderContent() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />;
+    }
+
+    return <Spinner message="Please accept location request" />;
   }
 
   //React says we have to define render!!
   render() {
-    return <div>Latitude: {this.state.lat}</div>;
+    return <div className="border-red">{this.renderContent()}</div>;
   }
 }
 
