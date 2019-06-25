bfe87e4bee4590ae9ef84a7d64d04a546edfa7c6
RobertS <rshugart@protonmail.com>
2019-06-24 22:10:44 -0400

    Refactored for Hooks

--- src/index.js
+++ src/index.js
@@ -1,37 +1,32 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import SeasonDisplay from "./SeasonDisplay";
-import Spinner from "./Spinner";
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
-class App extends React.Component {
-  state = { lat: null, errorMessage: "" }; // If we don't know what the value of a number will be, use null and the value will be passed in once it is determined.
+const App = () => {
+	const [ lat, setLat ] = useState(null);
+	const [ errorMessage, setErrorMessage ] = useState('');
 
-  componentDidMount() {
-    //Find users physical location with geolocation api in all browsers
-    window.navigator.geolocation.getCurrentPosition(
-      //success call back-found location
-      position => this.setState({ lat: position.coords.latitude }),
-      //Failure call back
-      err => this.setState({ errorMessage: err.message })
-    );
-  }
+	useEffect(() => {
+		//Find users physical location with geolocation api in all browsers
+		window.navigator.geolocation.getCurrentPosition(
+			//success call back-found location
+			(position) => setLat(position.coords.latitude),
+			//Failure call back
+			(err) => setErrorMessage(err.message)
+		);
+	}, []);
 
-  renderContent() {
-    if (this.state.errorMessage && !this.state.lat) {
-      return <div>Error: {this.state.errorMessage}</div>;
-    }
+	let content;
+	if (errorMessage) {
+		content = <div>Error: {errorMessage}</div>;
+	} else if (lat) {
+		content = <SeasonDisplay lat={lat} />;
+	} else {
+		content = <Spinner message="Please accept location request" />;
+	}
 
-    if (!this.state.errorMessage && this.state.lat) {
-      return <SeasonDisplay lat={this.state.lat} />;
-    }
+	return <div className="border-red">{content}</div>;
+};
 
-    return <Spinner message="Please accept location request" />;
-  }
-
-  //React says we have to define render!!
-  render() {
-    return <div className="border-red">{this.renderContent()}</div>;
-  }
-}
-
-ReactDOM.render(<App />, document.querySelector("#root"));
+ReactDOM.render(<App />, document.querySelector('#root'));
